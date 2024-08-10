a7487082cd669dfecfe24ce97919970e06ee6e9c
sorush <sorushshajari.0021@gmail.com>
2024-08-10 06:24:38 +0330

    add Navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { UserButton } from '@clerk/nextjs' 
+
 const Home = () => {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+     
     </div>
   )
 }
