1f8a6eab26155f0d0477056b86011624be394c6b
sorush <sorushshajari.0021@gmail.com>
2024-08-11 03:17:01 +0330

    add left sidebar

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -48,26 +48,23 @@ const MobileNav = () => {
            <NavContent />
         </SheetClose>
         <SignedOut>
-            <div className='flex flex-col gap-3'>
-                <SheetClose asChild>
-                    <Link href='/sign-in'>
-                     <Button className='small-medium btn-secondary
-                     min-h-[42px] w-full rounded-lg px-4 py-3 shadow-none'>
-                        <span className='primary-text-gradient'>
-                            Log In
-                        </span>
-                     </Button>
-                    </Link>
-                </SheetClose>
-                <SheetClose asChild>
-                    <Link href='/sign-up'>
-                     <Button className='small-medium light-border-2 btn-tertiary
-                     min-h-[42px] w-full rounded-lg px-4 py-3 shadow-none text-dark400_light900'>
-                            Sign Up 
-                     </Button>
-                    </Link>
-                </SheetClose>
-            </div>
+          <div className="flex flex-col gap-3">
+            <SheetClose asChild>
+              <Link href="/sign-in">
+                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <span className="primary-text-gradient">Log In</span>
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href="/sign-up">
+                <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none'>
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
         </SignedOut>
        </div>
     </SheetContent>
