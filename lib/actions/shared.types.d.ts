c34d07b72597ee77714e0b05e73289627d41162f
sorush <sorushshajari.0021@gmail.com>
2024-09-03 05:55:38 +0330

    assss

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -16,3 +16,22 @@ export interface createQuestionParams{
     author:Schema.Types.ObjectId | IUser ;
     path: string ;
 }
+
+export interface CreateUserParams {
+    clerkId: string;
+    name: string;
+    username: string;
+    email: string;
+    picture: string;
+  }
+
+
+  export interface UpdateUserParams {
+    clerkId: string;
+    updateData: Partial<IUser>;
+    path: string;
+  }
+
+  export interface DeleteUserParams {
+    clerkId: string;
+  }
