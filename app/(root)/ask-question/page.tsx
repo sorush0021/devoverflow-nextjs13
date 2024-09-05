f66fdaf0ed70617a3bd0d7df59fc6ddd39a233bc
sorush <sorushshajari.0021@gmail.com>
2024-09-05 05:24:57 +0330

    add middleware

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -15,7 +15,7 @@ const Page = async() => {
     <div>
       <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
       <div>
-        <Question mongoUserId={JSON.stringify(mongoUser._id)}/>
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)}/>
       </div>
       </div>
   )
