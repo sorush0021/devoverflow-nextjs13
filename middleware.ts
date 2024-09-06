063955f7ac8d1c72a9d403438012093bae198d28
sorush <sorushshajari.0021@gmail.com>
2024-09-06 04:35:29 +0330

    add

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,22 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
-
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+import { authMiddleware } from "@clerk/nextjs/server";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    'question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs'
   ],
-}
+  ignoredRoutes: [
+    '/api/webhook', '/api/chatgpt'
+  ]
+});
+ 
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+ 
